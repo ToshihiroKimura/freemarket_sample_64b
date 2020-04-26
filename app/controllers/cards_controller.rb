44042446be96457a68ba032074927f4132529407
ToshihiroKimura <toshi.kimura5903@gmail.com>
2020-04-26 22:29:53 +0900

    edit_cards_show

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -19,6 +19,7 @@ class CardsController < ApplicationController
       @card = Card.create(                  # カードテーブルのデータの作成
         user_id: current_user.id,
         customer_id: customer.id,
+        card_id: customer.default_card
       )
       if @card.save
         redirect_to root_path
@@ -41,8 +42,23 @@ class CardsController < ApplicationController
   # end
 
   def show #Cardのデータpayjpに送り情報を取り出す
-    card = Card.where(user_id: current_user.id).first
-    customer = Payjp::Customer.retrieve(card.customer_id)
-    # @default_card_information = customer.cards.retrieve(card.card_id)
+    @card = Card.where(user_id: current_user.id).first if Card.where(user_id: current_user.id).present?
+    customer = Payjp::Customer.retrieve(@card.customer_id)
+    @card_information = customer.cards.retrieve(@card.card_id)
+    @card_brand = @card_information.brand
+    case @card_brand
+    when "Visa"
+      @card_src = "visa.svg"
+    when "JCB"
+      @card_src = "jcb.svg"
+    when "MasterCard"
+      @card_src = "master-card.svg"
+    when "American Express"
+      @card_src = "american_express.svg"
+    when "Diners Club"
+      @card_src = "dinersclub.svg"
+    when "Discover"
+      @card_src = "discover.svg"
+    end
   end
 end
